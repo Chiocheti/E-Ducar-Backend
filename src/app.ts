e0f6e792e5db92f07f1b2a0f4869639fb6f41807
chiocheti <gabrielchiocheti@gmail.com>
2024-12-30 11:18:02 -0300

    fix(app): change app to get and send images

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import cors from 'cors';
 import express, { Request, Response } from 'express';
+import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req: Request, res: Response) => {
   res.redirect('/docs');
 });
+
 export default app;
