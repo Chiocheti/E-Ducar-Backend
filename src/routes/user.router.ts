83c19b628e124deabdcaa668ebdf17aa41813572
chiocheti <gabrielchiocheti@gmail.com>
2025-01-20 17:03:02 -0300

    feat(users): update functions to create and update users

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,20 +1,10 @@
 import { Router, Request, Response } from 'express';
 import UserController from '../controllers/User.controller';
 import multer from 'multer';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
 
 const userRoutes = Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..', 'uploads'));
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = `${uuidv4()}-${file.originalname}`;
-    cb(null, uniqueName);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
@@ -34,12 +24,14 @@ userRoutes.post('/create', upload.single('image'), async (req: Request, res: Res
   await UserController.create(req, res);
 });
 
+userRoutes.put('/update', async (req: Request, res: Response) => {
+  await UserController.update(req, res);
+});
+
 userRoutes.put('/updateImage', upload.single('image'), async (req: Request, res: Response) => {
   await UserController.updateImage(req, res);
 });
 
-userRoutes.put('/update', async (req: Request, res: Response) => {
-  await UserController.update(req, res);
-});
+
 
 export default userRoutes;
