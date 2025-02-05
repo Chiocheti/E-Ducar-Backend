a34576e85b1dcb8f18002cce83819892f496d308
chiocheti <gabrielchiocheti@gmail.com>
2025-02-05 16:20:39 -0300

    fix(routes): fix routes implemente new middleware

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import UserController from '../controllers/User.controller';
 import multer from 'multer';
+import authenticate from '../middlewares/auth';
 
 const userRoutes = Router();
 
@@ -8,30 +9,28 @@ const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
-userRoutes.get('/getAll', async (req: Request, res: Response) => {
+userRoutes.get('/getAll', authenticate, async (req: Request, res: Response) => {
   await UserController.getAll(req, res);
 });
 
-userRoutes.get('/getTeachers', async (req: Request, res: Response) => {
+userRoutes.get('/getTeachers', authenticate, async (req: Request, res: Response) => {
   await UserController.getTeachers(req, res);
 });
 
-userRoutes.post('/getById', async (req: Request, res: Response) => {
+userRoutes.post('/getById', authenticate, async (req: Request, res: Response) => {
   await UserController.getById(req, res);
 });
 
-userRoutes.post('/create', upload.single('image'), async (req: Request, res: Response) => {
+userRoutes.post('/create', authenticate, upload.single('image'), async (req: Request, res: Response) => {
   await UserController.create(req, res);
 });
 
-userRoutes.put('/update', async (req: Request, res: Response) => {
+userRoutes.put('/update', authenticate, async (req: Request, res: Response) => {
   await UserController.update(req, res);
 });
 
-userRoutes.put('/updateImage', upload.single('image'), async (req: Request, res: Response) => {
+userRoutes.put('/updateImage', authenticate, upload.single('image'), async (req: Request, res: Response) => {
   await UserController.updateImage(req, res);
 });
-
-
 
 export default userRoutes;
