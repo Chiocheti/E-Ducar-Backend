faf63d49928d99e749686a0e8c44f396e8152e5e
chiocheti <gabrielchiocheti@gmail.com>
2025-02-17 13:39:32 -0300

    feat(students): create function to edit a student

--- src/routes/student.router.ts
+++ src/routes/student.router.ts
@@ -1,15 +1,28 @@
 import { Router, Request, Response } from 'express';
 import StudentController from "../controllers/Student.controller";
 import authenticate from "../middlewares/auth";
+import multer from 'multer';
 
 const studentRoutes = Router();
 
-studentRoutes.post("/getById", async (req: Request, res: Response) => {
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
+
+studentRoutes.post("/getById", authenticate, async (req: Request, res: Response) => {
   await StudentController.getById(req, res);
 });
 
 studentRoutes.post("/create", async (req: Request, res: Response) => {
   await StudentController.create(req, res);
+});
+
+studentRoutes.put('/update', authenticate, async (req: Request, res: Response) => {
+  await StudentController.update(req, res);
+});
+
+studentRoutes.put('/updateImage', authenticate, upload.single('image'), async (req: Request, res: Response) => {
+  await StudentController.updateImage(req, res);
 });
 
 export default studentRoutes;
