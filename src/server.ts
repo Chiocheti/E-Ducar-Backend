7ab4eba3d610a3e6e7c18a9e8de04c92ba22351c
chiocheti <gabrielchiocheti@gmail.com>
2025-08-25 08:23:43 -0300

    feat(server): change unnecessary server functions to app

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
 import "dotenv/config";
 import app from "./app";
-import swaggerUI from "swagger-ui-express";
-import swaggerDocument from "./Docs/swagger";
-
-import routes from "./routes";
-
-app.use(routes);
-
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
